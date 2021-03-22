4fce0ee4780a59751e1ea065365a1676385badd9
Fatih Mehmet Dogan <fmehmetd29@gmail.com>
2021-03-22 13:13:14 +0300

    duzenlemee

--- footer.php
+++ footer.php
@@ -17,8 +17,8 @@
           <div class="col-lg-2 col-md-6 footer-links">
             <h4 style="color: white">Sayfalarımız</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a style="color: white" href="anasayfa">Anasayfa</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a style="color: white" href="hakkımızda">Hakkımızda</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a style="color: white" href="index">Anasayfa</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a style="color: white" href="hakkimizda">Hakkımızda</a></li>
               <li><i class="bx bx-chevron-right"></i> <a style="color: white" href="blog">Blog</a></li>
               <li><i class="bx bx-chevron-right"></i> <a style="color: white" href="iletisim">İletişim</a></li>
              
