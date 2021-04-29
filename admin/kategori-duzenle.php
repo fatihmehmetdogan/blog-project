7cac6d452f89ba628e5ef41f623a58081871193e
Fatih Mehmet Dogan <fmehmetd29@gmail.com>
2021-04-29 18:51:20 +0300

    kategori güncelleme sayfasının yapılması

--- admin/kategori-duzenle.php
+++ admin/kategori-duzenle.php
@@ -1,13 +1,14 @@
 <?php
 require_once 'header.php';
 require_once 'sidebar.php';
+require_once '../bootstrap.php';
+/* @var $entityManager Doctrine\ORM\EntityManager */
+$id = $_GET['id'];
+$CategoryRepository = $entityManager->getRepository('category');
+$category = $CategoryRepository->find($id);
 
-$kategorisor=$baglanti->prepare("SELECT * FROM kategori where kategori_id=:kategori_id");
-$kategorisor->execute(array(
-'kategori_id'=>$_GET['id']
-));
-$kategoricek=$kategorisor->fetch(PDO::FETCH_ASSOC);
- ?>
+
+    ?>
 <div class="content-wrapper">
     <section class="content">
       <div class="container-fluid">
@@ -18,11 +19,12 @@ $kategoricek=$kategorisor->fetch(PDO::FETCH_ASSOC);
               <div class="card-header">
                 <h3 class="card-title">kategori</h3>
               </div>
+
               <form action="islem.php" method="post" enctype="multipart/form-data">
                 <div class="card-body">
                          <div class="form-group">
                     <label for="exampleInputEmail1">kategori Resim</label>
-                    <img style="width:150px" src="resimler/kategori/<?php echo $kategoricek['kategori_resim'] ?>">
+                    <img style="width:150px" src="resimler/kategori/<?php echo $category->getCategoryImage() ?>">
                   </div>
                   <div class="form-group">
                     <label for="exampleInputEmail1">kategori Resim</label>
@@ -30,17 +32,18 @@ $kategoricek=$kategorisor->fetch(PDO::FETCH_ASSOC);
                   </div>
                    <div class="form-group">
                     <label for="exampleInputEmail1">kategori başlık</label>
-                    <input value="<?php echo $kategoricek['kategori_baslik'] ?>" name="baslik"  type="text" class="form-control"  placeholder="Lütfen başlık  giriniz.">
+                    <input value="<?php echo $category->getCategoryTitle() ?>" name="baslik"  type="text" class="form-control"  placeholder="Lütfen başlık  giriniz.">
                   </div>
-          <input type="hidden" name="katid" value="<?php echo $kategoricek['kategori_id']?>">
+          <input type="hidden" name="katid" value="<?php echo $category->getId()?>">
                     <div class="form-group">
                     <label for="exampleInputEmail1">kategori sıra</label>
-                    <input value="<?php echo $kategoricek['kategori_sira'] ?>" name="sira"  type="text" class="form-control"  placeholder="Lütfen sıra  giriniz.">
+                    <input value="<?php echo $category->getCategoryOrder() ?>" name="sira"  type="text" class="form-control"  placeholder="Lütfen sıra  giriniz.">
                   </div> 
 
-                  <input type="hidden" name="id" value="<?php echo $kategoricek['kategori_id'] ?>">
+                  <input type="hidden" name="id" value="<?php echo $category->getId() ?>">
                 </div>
-                <!-- /.card-body -->
+
+                  <!-- /.card-body -->
                 <div class="card-footer">
                   <button name="kategoriduzenle" style="float:right" type="submit" class="btn btn-primary">Kaydet</button>
                 </div>
